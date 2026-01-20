77a44554ad3ab4c6a59a1885193202e13e06c8b3
BillMillerCoding <billmillercoding@outlook.com>
2026-01-20 12:41:20 -0800

    basic Vue and wordle setup

---
+++ WordleWeb/app/app.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <v-btn variable="flat" color="primary">Hello</v-btn>
+  </v-container>
+</template>
+<script setup lang="ts">
+  const guesses: Ref<Array<Array<Letter>>> = ref([]);
+  const targetWord = ref("");
+  const guess = ref("");
+
+  function submit() {
+
+  }
+  class Letter {
+    character: string;
+    state: LetterState;
+    constructor(character: string, state: LetterState) {
+      this.character = character;
+      this.state = state;
+    }
+
+  }
+  enum LetterState {
+    Correct,
+    Misplaced,
+    Wrong,
+  }
+</script>
